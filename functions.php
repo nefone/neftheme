c2008947f1861fa940549f6a49f74cb624511168
Alexandr Toropov <contact@nefone.ru>
2016-09-06 15:56:44 +0300

    bootstrap version fix

--- functions.php
+++ functions.php
@@ -144,7 +144,7 @@ require get_template_directory() . '/inc/jetpack.php';
 function bootstrap_inc() {
     wp_register_style('bootstrap-style', get_template_directory_uri().'/css/bootstrap.min.css');
     wp_enqueue_style('bootstrap-style');
-    wp_register_script('bootstrap-js', get_template_directory_uri().'/js/bootstrap.min.js', array(), '3.3.6', true);
+    wp_register_script('bootstrap-js', get_template_directory_uri().'/js/bootstrap.min.js', array(), '3.3.7', true);
     wp_enqueue_script('bootstrap-js');
 }
 
